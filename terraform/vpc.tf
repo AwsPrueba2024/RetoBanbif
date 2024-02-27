0c48c047387ee8468445cb064b8b12ff668802d8
root <root@ip-172-31-31-225.ec2.internal>
2024-02-27 05:55:06 +0000

    update

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -31,16 +31,16 @@ resource "aws_security_group" "sg" {
 
   ingress {
     description = "https"
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "http"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
