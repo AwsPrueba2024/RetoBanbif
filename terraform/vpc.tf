b66e0db4caa361688111680d681271622f371ab6
root <root@ip-172-31-31-225.ec2.internal>
2024-02-27 07:50:02 +0000

    update

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -51,7 +51,7 @@ resource "aws_security_group" "sg" {
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]   
-
+  }
   egress {
     from_port   = 0
     to_port     = 0
